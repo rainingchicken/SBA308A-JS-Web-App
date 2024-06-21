5d0b2e46f9ad6de463e662634d58c8d63c26b1cc
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-21 00:12:22 -0700

    styled in js main typing test elements and added readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# SBA
+
+## Requirement
+
+- Cache at least one element using selectElementById.
+  - `const app = document.getElementById("app");`
+- Cache at least one element using querySelector or querySelectorAll.
+  - `const testClass = document.querySelectorAll(".test");`
+- Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+  - `const all = app.childrens;`
+- Iterate over a collection of elements to accomplish some task.
+  - `for (const i of testClass) {Object.assign(i.style, {...});}`
+- Create at least one element using createElement.
+  - `const h1 = document.createElement("h1");`
+- Use appendChild and/or prepend to add new elements to the DOM.
+  - `timerdiv.appendChild(minutesdiv);`
+- Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+  - when user click starts `writingPrompt.textContent` changes into 10 random quotes
+- Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+  - if user typed wrong, the word in the prompt will change color
+- Modify at least one attribute of an element in response to user interaction.
+  -when user clicks start button, user can now begin typing in the textarea which would otherwise be disabled `textarea.disabled = true;`
+- Register at least two different event listeners and create the associated event handler functions.
+  - `btn.addEventListener("click", handleTimer);`
+- Use at least two Browser Object Model (BOM) properties or methods.
+  - `textarea.focus();`
+- Include at least one form and/or input with HTML attribute validation.
+- Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+- Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+  - there are no errors
--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 //select elements
 const app = document.getElementById("app");
-const all = document.querySelectorAll("*");
+const all = app.childrens;
 
 //create elemetns
+const h1 = document.createElement("h1");
 const timerdiv = document.createElement("div");
-const minutesdiv = document.createElement("div");
-const secondsdiv = document.createElement("div");
+const minutesdiv = document.createElement("span");
+const secondsdiv = document.createElement("span");
 const buttonsdiv = document.createElement("div");
 const resultdiv = document.createElement("div");
 const btn = document.createElement("button");
 const writingPrompt = document.createElement("div");
 const textarea = document.createElement("textarea");
-
+const testcontainer = document.createElement("div");
+const testrow = document.createElement("div");
+const footer = document.createElement("footer");
 //initializations
 // https://gist.github.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80
 const getQuotes = () => {
@@ -20,6 +23,7 @@ const getQuotes = () => {
       return res.json();
     })
     .then((data) => {
+      writingPrompt.textContent = "";
       for (let index = 0; index < 10; index++) {
         let rand = Math.floor(Math.random() * 100);
         writingPrompt.textContent += data.quotes[rand].quote + " ";
@@ -27,23 +31,33 @@ const getQuotes = () => {
     })
     .catch((error) => console.error("Unable to fetch data:", error));
 };
-getQuotes();
-let string = "rnaodm random random";
+
 let minutes = 0;
 let seconds = 60;
 let intervalID;
 let isBtnDisabled = false;
 
 //attributes
-writingPrompt.setAttribute("type", "text");
-
-textarea.setAttribute("cols", 50);
-textarea.setAttribute("rows", 20);
 textarea.disabled = true; //can't start typing before timer
-minutesdiv.textContent = "1:";
+writingPrompt.setAttribute("type", "text");
+writingPrompt.textContent =
+  "The prompt in which you will copy via typing will appear here. Try to type as fast and accurate as you can!";
+textarea.setAttribute("placeholder", "Click START and type here!");
+h1.textContent = "1-minute Typing Test";
+btn.textContent = "START";
+minutesdiv.textContent = "01:";
 secondsdiv.textContent = "00";
+resultdiv.textContent = "Result";
+footer.innerHTML = `<a>https://github.com/rainingchicken/SBA316-DOM-Typing-Test</a>`;
 
-resultdiv.textContent = "result";
+//class
+writingPrompt.classList.add("test");
+textarea.classList.add("test");
+writingPrompt.classList.add("col");
+textarea.classList.add("col");
+testrow.classList.add("row");
+testcontainer.classList.add("container");
+app.classList.add("font");
 
 const calculateWPM = () => {
   let count = 0;
@@ -57,20 +71,37 @@ const calculateWPM = () => {
     if (word === wordPrompt) {
       //count word if input and prompt words are equal
       count++;
+    } else {
+      writingPrompt.innerHTML = writingPrompt.innerHTML.replace(
+        wordPrompt,
+        `<span style='color: #BD8E83'>${wordPrompt}</span>`
+      );
     }
-    //add changing color of mistyping here
   }
-
   resultdiv.innerHTML = count + " WPM";
 };
 
 const updateTimer = () => {
+  minutesdiv.textContent = "00:";
+
   if (seconds === 0) {
     //when times up
+    let audio = new Audio(
+      "https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg"
+    );
+    audio.play();
     clearInterval(intervalID); //stop timer
     calculateWPM(); //calculate
     textarea.disabled = true; //can't type futher
     isBtnDisabled = false;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (seconds < 5) {
+    timerdiv.style.color = "#BD8E83";
+  } else {
+    timerdiv.style.color = "black";
   }
   secondsdiv.textContent = seconds;
   seconds--;
@@ -80,46 +111,119 @@ const handleTimer = () => {
   textarea.disabled = false;
   textarea.focus();
   textarea.value = "";
+  getQuotes();
   if (!isBtnDisabled) {
-    seconds = 60;
-    intervalID = setInterval(updateTimer, 500);
+    seconds = 59;
+    intervalID = setInterval(updateTimer, 50);
   }
   isBtnDisabled = true; //can't click on timer during countdown to prevent weird setInterval
 };
 
-//appends
-// timerdiv.appendChild(minutesdiv);
-
 btn.addEventListener("click", handleTimer);
 
 //appends to app
-app.appendChild(timerdiv);
-app.appendChild(btn);
-app.appendChild(resultdiv);
-app.appendChild(writingPrompt);
-app.appendChild(textarea);
+timerdiv.appendChild(minutesdiv);
 timerdiv.appendChild(secondsdiv);
+buttonsdiv.appendChild(btn);
+testrow.appendChild(writingPrompt);
+testrow.appendChild(textarea);
+testcontainer.appendChild(testrow);
+app.appendChild(h1);
+app.appendChild(timerdiv);
+app.appendChild(buttonsdiv);
+app.appendChild(resultdiv);
+app.appendChild(testcontainer);
+app.appendChild(footer);
 //css
-for (const iterator of all) {
-  Object.assign(iterator.style, {
-    boxSizing: "border-box",
-    // display: "flex",
+const testClass = document.querySelectorAll(".test");
+const fontClass = document.querySelectorAll(".font");
+
+//https://www.canva.com/colors/color-palettes/classic-travel/
+const bgColor = "#E9DAC4";
+const boxColor = "white";
+const boxTexture =
+  "url(https://images.pexels.com/photos/20818860/pexels-photo-20818860/free-photo-of-white-wrinkled-paper-sheet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
+const btnColor = "#749EB2";
+const shadow = "#AC8E60";
+
+for (const i of fontClass) {
+  Object.assign(i.style, {
+    fontFamily: "'Chakra Petch', sans-serif",
+    fontWeight: "400",
+    fontStyle: "normal",
   });
 }
 
+document.body.style.backgroundColor = bgColor;
+
 Object.assign(app.style, {
-  backgroundColor: "gray",
-  width: "100%",
-  height: "100vh",
-  margin: "0",
-  padding: "0",
-});
-Object.assign(writingPrompt.style, {
-  backgroundColor: "yellow",
-  width: "70%",
-  height: "30vh",
+  marginTop: "0",
 });
 
-Object.assign(textarea.style, {
+for (const i in all) {
+  Object.assign(all[i].style, {
+    boxSizing: "border-box",
+    // display: "flex",
+    // flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  });
+}
+
+Object.assign(h1.style, {
+  textAlign: "center",
+  margin: "1em auto",
+  marginBottom: "0",
+});
+
+Object.assign(timerdiv.style, {
+  display: "inline-block",
+  fontSize: "3em",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  margin: "0 auto",
+  width: "100%",
+});
+Object.assign(buttonsdiv.style, {
+  textAlign: "center",
+
+  width: "100%",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  margin: "0 auto",
+  display: "flex",
+});
+Object.assign(btn.style, {
+  padding: "1em",
+  borderRadius: "20px",
+  margin: ".5em auto",
+  border: "none",
+  backgroundColor: btnColor,
+});
+Object.assign(resultdiv.style, {
+  textAlign: "center",
+  margin: "0 auto",
+  fontSize: "2em",
+});
+for (const i of testClass) {
+  Object.assign(i.style, {
+    //background: boxTexture,
+    backgroundColor: boxColor,
+    backgroundBlendMode: "multiply",
+    padding: "2em",
+    margin: ".5em",
+    fontSize: "1.5em",
+    lineHeight: "1.5",
+    borderRadius: "20px",
+    boxShadow: "10px 5px 5px" + shadow,
+  });
+}
+Object.assign(footer.style, {
+  textAlign: "center",
+  margin: "0 auto",
+  width: "100%",
+  bottom: "0",
   backgroundColor: "red",
 });
